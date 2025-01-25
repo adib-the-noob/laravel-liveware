f67f6d96718aecdceea09f55cfabe275a40f0521
Mohammed Adib <mohammed.adib1104@gmail.com>
2025-01-26 03:33:38 +0600

    a simple component added

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -7,6 +7,11 @@ use Livewire\Component;
 class Greeter extends Component
 {
     public $name = "ADIB";
+
+    public function changeName($name){
+        $this->name = $name;
+    }
+    
     public function render()
     {
         return view('livewire.greeter');
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,3 +1,13 @@
 <div>
-    Hello, {{ $name}}!
+    <div>
+        Hello, {{ $name}}!
+    </div>
+    <div class="mt-2">
+        <button 
+            class="bg-slate-400 text-black font-medium rounded-md p-2" 
+            wire:click="changeName('ADIB[changed]')"
+        >
+            Change Name!
+        </button>
+    </div>
 </div>
