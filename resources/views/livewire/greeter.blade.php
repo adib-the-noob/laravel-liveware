ff16dbdb899f78cabfacb6eb5eb230c8073ca233
Mohammed Adib <mohammed.adib1104@gmail.com>
2025-01-27 01:56:49 +0600

    tailwind configured

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,13 +1,22 @@
 <div>
-    <div>
+    <div class="bg-yellow-300 rounded p-2">
         Hello, {{ $name}}!
     </div>
+    <form wire:submit="changeName(document.querySelector('#newName').value)">
     <div class="mt-2">
+    <input
+        id="newName"    
+        type="text" 
+        class="w-full p-4 border rounded-md bg-red-300 text-black m-2"
+        >
+    </div>
+    <div class="mt-2 mx-2 my-2">
         <button 
-            class="bg-slate-400 text-black font-medium rounded-md p-2" 
-            wire:click="changeName('ADIB[changed]')"
+            type="submit"
+            class="bg-slate-400 text-black font-medium rounded-md p-2 m-4" 
         >
             Change Name!
         </button>
     </div>
+    </form>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])    
+<head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -9,13 +10,17 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="antialiased">
-        
-        <main class="mt-6">
-            <livewire:greeter />
-        </main>
+    <body class="antialiased bg-gray-50">
+        <div class="container mx-auto px-4">
+            <main class="mt-6 max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-6">
+                <!-- Color display element -->
+                <div class="bg-blue-500 text-white p-4 rounded mb-6">
+                    <p class="font-bold text-lg text-white">HackerOne</p>
+                </div>
+                
+                <livewire:greeter />
+            </main>
+        </div>
     </body>
 </html>
